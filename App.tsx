e25298078437c31e0da0e498e63b26a364d09d1e
Qulbe12 <qulbehussain@outlook.com>
2023-02-24 13:53:58 +0500

    Fix: converted into expo app with no errors and successfully running with typescript

--- App.tsx
+++ App.tsx
@@ -1,6 +1,19 @@
 import Navigation from "./src/navigation/AppNavigation";
+import React, {useCallback} from "react";
+import { useFonts } from 'expo-font';
+
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Lato-Black': require('./src/fonts/Lato-Black.ttf'),
+    'Lato-Bold': require('./src/fonts/Lato-Bold.ttf'),
+    'Lato-Regular': require('./src/fonts/Lato-Regular.ttf'),
+    'Rancho-Regular': require('./src/fonts/Rancho-Regular.ttf'),
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
   return  <Navigation/>;
 }
 
